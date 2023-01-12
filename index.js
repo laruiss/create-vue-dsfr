a234d4350ec58f9fbd2ac38575eb16737dd75087
Stanislas Ormières <stan@stormier.ninja>
2023-01-12 13:23:16 +0100

    fix: :bug: Corrige les imports

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 
 // @ts-check
-const fs = require('fs')
 const path = require('path')
+const { mkdir, stat } = require('node:fs/promises')
+const fs = require('node:fs')
+
+const AdmZip = require('adm-zip')
+const { $fetch } = require('ohmyfetch')
+const rimraf = require('rimraf')
 // Avoids autoconversion to number of the project name by defining that the args
 // non associated with an option ( _ ) needs to be parsed as a string. See #4606
 const argv = require('minimist')(process.argv.slice(2), { string: ['_'] })
@@ -19,8 +24,6 @@ const {
 } = require('kolorist')
 
 const cwd = process.cwd()
-
-const { getLatestDsfrRelease } = require('./fetch-dsfr')
 
 const FRAMEWORKS = [
   {
@@ -289,6 +292,86 @@ function pkgFromUserAgent(userAgent) {
     name: pkgSpecArr[0],
     version: pkgSpecArr[1]
   }
+}
+
+
+const dsfrTmpDir = './tmp' + Math.random().toString().substring(2, 10)
+
+async function getLatestDsfrRelease(targetFolder = './public/icons', owner = 'GouvernementFR', repo = 'dsfr') {
+  const releaseInfo = await $fetch(
+    `https://api.github.com/repos/${owner}/${repo}/releases/latest`,
+    {
+      headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0'}
+    }
+  )
+  const asset = releaseInfo.assets[0]
+  const assetName = asset.name
+  const tmpDir = path.resolve(dsfrTmpDir)
+  const statTmpDir = await stat(tmpDir).catch(() => null)
+  if (!statTmpDir || !statTmpDir.isDirectory()) {
+    await mkdir(tmpDir)
+  }
+  const assetPath = path.resolve(dsfrTmpDir, `${assetName}`)
+  await downloadFile(asset.browser_download_url, assetPath)
+  const statZipFile = await stat(assetPath)
+  await extractIcons(assetPath, targetFolder)
+  return new Promise((resolve, reject) => {
+    console.log(yellow('⧖ Nettoyage en cours...'))
+    rimraf(dsfrTmpDir, (err) => {
+      if (err) {
+        console.log(red('✖') + ' ' + err.message)
+        reject(err)
+      }
+      console.log(green('✔ Nettoyage effectué'))
+      resolve()
+    })
+  })
+}
+
+async function downloadFile(url, filepath) {
+  const blob = await $fetch(
+    url,
+    {
+      headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0'},
+      responseType: 'blob',
+    }
+  )
+    
+  return new Promise((resolve, reject) => {
+    const filePath = fs.createWriteStream(filepath)
+    filePath.on('error', (err) => {
+      filePath.close();
+      reject(err)
+    })
+    filePath.on('finish',() => {
+      filePath.close();
+      resolve()
+    })
+    blob.arrayBuffer().then(arrayBuffer => {
+      filePath.write(Buffer.from(arrayBuffer))
+      console.log(green('✔ Icones téléchargées'));
+      resolve()
+    })
+  })
+}
+
+async function extractIcons (filepath, targetFolder) {
+  targetFolder += targetFolder.endsWith('/') ? '' : '/'
+  const zip = new AdmZip(filepath)
+  console.log(yellow(`⧖ Extraction des icônes en cours (dans ${lightGray(targetFolder)})...`))
+  zip
+    .getEntries()
+    .map(({entryName}) => entryName)
+    .filter(entryName => entryName.includes('dist/icons/') && entryName.endsWith('.svg'))
+    .forEach(entryName => {
+      const target = path.dirname(targetFolder + entryName.replace(/.*dist\/icons\/(.*)/, '$1'))
+      try {
+        zip.extractEntryTo(entryName, target, /*maintainEntryPath*/ false, /*overwrite*/ true)
+      } catch (e) {
+        console.log(`${red('✖')} Erreur dans le dossier ${target}: ` + e.message)
+      }
+    })
+    console.log(green(`✔ Icônes extraites dans ${yellow(targetFolder)}`))
 }
 
 init().catch((e) => {
--- update-icons.js
+++ update-icons.js
@@ -4,10 +4,9 @@
 const path = require('path')
 const { stat } = require('node:fs/promises')
 
-const { red, yellow } = require('kolorist')
+const { green, red, yellow } = require('kolorist')
+const { $fetch } = require('ohmyfetch')
 const prompts = require('prompts')
-
-const { getLatestDsfrRelease } = require('./fetch-dsfr.js')
 
 const pkgPath = path.resolve(process.cwd(), 'package.json')
 
@@ -51,6 +50,85 @@ async function init() {
     },
   ])
   getLatestDsfrRelease(targetDir, 'GouvernementFR', 'dsfr')
+}
+
+const dsfrTmpDir = './tmp' + Math.random().toString().substring(2, 10)
+
+async function getLatestDsfrRelease(targetFolder = './public/icons', owner = 'GouvernementFR', repo = 'dsfr') {
+  const releaseInfo = await $fetch(
+    `https://api.github.com/repos/${owner}/${repo}/releases/latest`,
+    {
+      headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0'}
+    }
+  )
+  const asset = releaseInfo.assets[0]
+  const assetName = asset.name
+  const tmpDir = path.resolve(dsfrTmpDir)
+  const statTmpDir = await stat(tmpDir).catch(() => null)
+  if (!statTmpDir || !statTmpDir.isDirectory()) {
+    await mkdir(tmpDir)
+  }
+  const assetPath = path.resolve(dsfrTmpDir, `${assetName}`)
+  await downloadFile(asset.browser_download_url, assetPath)
+  const statZipFile = await stat(assetPath)
+  await extractIcons(assetPath, targetFolder)
+  return new Promise((resolve, reject) => {
+    console.log(yellow('⧖ Nettoyage en cours...'))
+    rimraf(dsfrTmpDir, (err) => {
+      if (err) {
+        console.log(red('✖') + ' ' + err.message)
+        reject(err)
+      }
+      console.log(green('✔ Nettoyage effectué'))
+      resolve()
+    })
+  })
+}
+
+async function downloadFile(url, filepath) {
+  const blob = await $fetch(
+    url,
+    {
+      headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0'},
+      responseType: 'blob',
+    }
+  )
+    
+  return new Promise((resolve, reject) => {
+    const filePath = fs.createWriteStream(filepath)
+    filePath.on('error', (err) => {
+      filePath.close();
+      reject(err)
+    })
+    filePath.on('finish',() => {
+      filePath.close();
+      resolve()
+    })
+    blob.arrayBuffer().then(arrayBuffer => {
+      filePath.write(Buffer.from(arrayBuffer))
+      console.log(green('✔ Icones téléchargées'));
+      resolve()
+    })
+  })
+}
+
+async function extractIcons (filepath, targetFolder) {
+  targetFolder += targetFolder.endsWith('/') ? '' : '/'
+  const zip = new AdmZip(filepath)
+  console.log(yellow(`⧖ Extraction des icônes en cours (dans ${lightGray(targetFolder)})...`))
+  zip
+    .getEntries()
+    .map(({entryName}) => entryName)
+    .filter(entryName => entryName.includes('dist/icons/') && entryName.endsWith('.svg'))
+    .forEach(entryName => {
+      const target = path.dirname(targetFolder + entryName.replace(/.*dist\/icons\/(.*)/, '$1'))
+      try {
+        zip.extractEntryTo(entryName, target, /*maintainEntryPath*/ false, /*overwrite*/ true)
+      } catch (e) {
+        console.log(`${red('✖')} Erreur dans le dossier ${target}: ` + e.message)
+      }
+    })
+    console.log(green(`✔ Icônes extraites dans ${yellow(targetFolder)}`))
 }
 
 init().catch((e) => {
